51be8dd9c7d5813a13f8cc2b05f2b982279973f3
zhiwei <1178679449@qq.com>
2025-11-04 18:42:53 +0800

    chore: 更新构建脚本使用 docker compose 替代 docker-compose
    使用新版 docker compose 命令替代旧版 docker-compose，保持与最新 docker 版本兼容

--- build-gpu.sh
+++ build-gpu.sh
@@ -1,5 +1,5 @@
 git pull
 echo y |docker image prune
-docker-compose down --remove-orphans
-docker-compose -f 'docker-compose-gpu.yml' up -d --build kn
-docker-compose logs -f --tail 100 kn
+# docker compose down --remove-orphans
+docker compose -f 'docker-compose-gpu.yml' up -d --build kn
+docker compose logs -f --tail 100 kn
